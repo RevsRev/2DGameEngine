2a4d5f7fc4bdd5b548d8d7616071715dfeb5e8a3
RevsRev <eddie.revell@yahoo.co.uk>
2021-04-19 21:44:44 +0100

    Improvements to dialogs

--- src/Graphics/ui/Panels/RvPhysicalObjectCreator.cs
+++ src/Graphics/ui/Panels/RvPhysicalObjectCreator.cs
@@ -1,13 +1,25 @@
 using Microsoft.Xna.Framework;
 
-public class RvPhysicalObjectCreator : RvPanel
+public class RvPhysicalObjectCreator : RvDialogPanel
 {
+    private const int DEFAULT_WIDTH = 400;
+    private const int DEFAULT_HEIGHT = 200;
+
+    //This is not ideal but will do for now.
+    private readonly Rectangle TIP_TEXT_POSITIONING = new Rectangle(5, 5, 90, 20);
+    private readonly Rectangle TEXT_FIELD_POSITIONING = new Rectangle(5, 30, 90, 65);
+
     private RvText tipText;
     private RvTextField textField;
 
     public RvPhysicalObjectCreator(Rectangle bounds) : base(bounds)
     {
         initComponents();
+    }
+
+    public RvPhysicalObjectCreator(Vector2 position) : this(new Rectangle((int)position.X, (int)position.Y, DEFAULT_WIDTH, DEFAULT_HEIGHT))
+    {
+
     }
 
     private void initComponents()
@@ -19,20 +31,21 @@ public class RvPhysicalObjectCreator : RvPanel
         addComponent(textField);
     }
 
-    private Rectangle getTipTextBounds()
+    private Rectangle getAdjustedBounds(Rectangle boundsToAdjust)
     {
-        int height = (int)(0.9 * bounds.Height/2);
-        int width = (int)(0.9 * bounds.Width);
-        int x = (int)(0.05*bounds.Width + bounds.X);
-        int y = (int)(0.05*bounds.Height/2 + bounds.Y);
+        Rectangle contentBounds = getContentBounds();
+        int width = (int)(contentBounds.Width * (boundsToAdjust.Width/(double)100));
+        int height = (int)(contentBounds.Height * (boundsToAdjust.Height/(double)100));
+        int x = (int)(contentBounds.Width * boundsToAdjust.X/(double)100);
+        int y = (int)(contentBounds.Height * boundsToAdjust.Y/(double)100);
         return new Rectangle(x, y, width, height);
+    }
+    private Rectangle getTipTextBounds()
+    {        
+        return getAdjustedBounds(TIP_TEXT_POSITIONING);
     }
     private Rectangle getTextFieldBounds()
     {
-        int height = (int)(0.9 * bounds.Height/2);
-        int width = (int)(0.9 * bounds.Width);
-        int x = (int)(0.05*bounds.Width + bounds.X);
-        int y = (int)(bounds.Height/2 + 0.05*bounds.Height/2 + bounds.Y);
-        return new Rectangle(x, y, width, height);
+        return getAdjustedBounds(TEXT_FIELD_POSITIONING);
     }
 }
